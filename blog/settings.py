14d213cd4088e092d860f65892f566c6498d2334
Danilo Santos <danilocraft933@gmail.com>
2025-02-26 22:53:47 -0300

    Configure railway

--- blog/settings.py
+++ blog/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['daniblogs-production.up.railway.app']
+ALLOWED_HOSTS = ['https://daniblogs-production.up.railway.app/']
 
 CSRF_TRUSTED_ORIGINS = ['https://daniblogs-production.up.railway.app']
 
